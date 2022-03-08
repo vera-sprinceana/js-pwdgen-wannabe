9af3c881d77270e120fbb05d5cfc1753e665f51e
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-03-08 16:58:30 +0100

    password generator

---
+++ myscript.js
@@ -0,0 +1,16 @@
+const nome= prompt("ciao, qual'è il tuo nome?");
+console .log ( nome );
+
+const cognome= prompt("qual'è il tuo cognome");
+console .log ( cognome );
+
+const colorepreferito= prompt("quale colore preferisci?");
+console .log ( colorepreferito );
+
+let numero= prompt ("numero preferito?");
+number=parseInt(numero);
+console .log ( numero );
+
+let frase= `nomecognomecolorepreferito21`;
+console .log (frase);
+document.getElementById("frase").innerHTML=frase;
